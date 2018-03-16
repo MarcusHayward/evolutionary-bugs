99008cc86ee21408d1a632da7901335b62abe0bb
Marcus Hayward <mhayward@inviqa.com>
2018-03-16 15:15:30 +0000

    Cover up, down, left, right and not moving when at boundries with tests

--- src/test/scala/WorldSpec.scala
+++ src/test/scala/WorldSpec.scala
@@ -3,16 +3,95 @@ package bugs
 import org.scalatest.{FlatSpec, Matchers}
 
 class WorldSpec extends FlatSpec with Matchers {
-  it should "moves the player down in the world" in {
-    val world = World.generateRandom(10)
-    val player = Piece(Player(100, 100), (0, 0))
-    val worldWithPlayer = world.withPlayer(player)
+  val world: World = World.generateRandom(10)
+  val player: Piece = Piece(Player(100, 100), (5, 5))
+  val worldWithPlayer: World = world.withPlayer(player)
 
+  it should "moves the player down in the world" in {
     val newWorld = worldWithPlayer.movePlayer(Down, player)
 
-    newWorld._2.coordinates shouldBe((1, 0))
+    newWorld._2.coordinates shouldBe((6, 5))
 
-    val filteredPlayer: List[Piece] = newWorld._1.pieces.filter((piece: Piece) => piece.coordinates == (1, 0))
+    val filteredPlayer: List[Piece] = newWorld._1.pieces.filter((piece: Piece) => piece.coordinates == (6, 5))
+
+    filteredPlayer shouldBe List(newWorld._2)
+  }
+
+  it should "moves the player up in the world" in {
+    val newWorld = worldWithPlayer.movePlayer(Up, player)
+
+    newWorld._2.coordinates shouldBe((4, 5))
+
+    val filteredPlayer: List[Piece] = newWorld._1.pieces.filter((piece: Piece) => piece.coordinates == (4, 5))
+
+    filteredPlayer shouldBe List(newWorld._2)
+  }
+
+  it should "moves the player left in the world" in {
+    val newWorld = worldWithPlayer.movePlayer(Left, player)
+
+    newWorld._2.coordinates shouldBe((5, 4))
+
+    val filteredPlayer: List[Piece] = newWorld._1.pieces.filter((piece: Piece) => piece.coordinates == (5, 4))
+
+    filteredPlayer shouldBe List(newWorld._2)
+  }
+
+  it should "moves the player right in the world" in {
+    val newWorld = worldWithPlayer.movePlayer(Right, player)
+
+    newWorld._2.coordinates shouldBe((5, 6))
+
+    val filteredPlayer: List[Piece] = newWorld._1.pieces.filter((piece: Piece) => piece.coordinates == (5, 6))
+
+    filteredPlayer shouldBe List(newWorld._2)
+  }
+
+  it should "not move the player down in the world when at the bottom of the map" in {
+    val player: Piece = Piece(Player(100, 100), (9, 5))
+    val worldWithPlayer: World = world.withPlayer(player)
+    val newWorld = worldWithPlayer.movePlayer(Down, player)
+
+    newWorld._2.coordinates shouldBe((9, 5))
+
+    val filteredPlayer: List[Piece] = newWorld._1.pieces.filter((piece: Piece) => piece.coordinates == (9, 5))
+
+    filteredPlayer shouldBe List(newWorld._2)
+  }
+
+
+  it should "not move the player up in the world when at the top of the map" in {
+    val player: Piece = Piece(Player(100, 100), (0, 5))
+    val worldWithPlayer: World = world.withPlayer(player)
+    val newWorld = worldWithPlayer.movePlayer(Up, player)
+
+    newWorld._2.coordinates shouldBe((0, 5))
+
+    val filteredPlayer: List[Piece] = newWorld._1.pieces.filter((piece: Piece) => piece.coordinates == (0, 5))
+
+    filteredPlayer shouldBe List(newWorld._2)
+  }
+
+  it should "not move the player left in the world when at the left most side of the map" in {
+    val player: Piece = Piece(Player(100, 100), (5, 0))
+    val worldWithPlayer: World = world.withPlayer(player)
+    val newWorld = worldWithPlayer.movePlayer(Left, player)
+
+    newWorld._2.coordinates shouldBe((5, 0))
+
+    val filteredPlayer: List[Piece] = newWorld._1.pieces.filter((piece: Piece) => piece.coordinates == (5, 0))
+
+    filteredPlayer shouldBe List(newWorld._2)
+  }
+
+  it should "not move the player right in the world when at the right most side of the map" in {
+    val player: Piece = Piece(Player(100, 100), (5, 9))
+    val worldWithPlayer: World = world.withPlayer(player)
+    val newWorld = worldWithPlayer.movePlayer(Right, player)
+
+    newWorld._2.coordinates shouldBe((5, 9))
+
+    val filteredPlayer: List[Piece] = newWorld._1.pieces.filter((piece: Piece) => piece.coordinates == (5, 9))
 
     filteredPlayer shouldBe List(newWorld._2)
   }
